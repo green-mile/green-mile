c136c021a9f3f997954576a531f1a3ceb9c48a40
Declan <declanisaackuok@gmail.com>
2023-01-02 16:51:51 +0800

    Added transfer household functionality

---
+++ GreenMile/src/Web/UiState/TransferHouseholdUiState.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.UiState
+{
+    public class TransferHouseholdUiState
+    {
+        [BindProperty]
+        public string? InviteLink { get; set; }
+
+        [BindProperty]
+        public string? CreateHouseholdName { get; set; }
+
+        [BindProperty]
+        public bool? JoinHousehold { get; set; } = true;
+
+        [BindProperty]
+        public string? Address { get; set; } = null;
+    }
+}
