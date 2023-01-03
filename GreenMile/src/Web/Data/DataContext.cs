ba0919ce9bbfcc3c8064fc2762354bb39b85029e
Luth Andyka <luthandyka.business@gmail.com>
2023-01-03 22:03:06 +0800

    Initialise FoodTracker models

--- GreenMile/src/Web/Data/DataContext.cs
+++ GreenMile/src/Web/Data/DataContext.cs
@@ -8,6 +8,9 @@ namespace Web.Data;
 public class DataContext : IdentityDbContext<User>
 {
     public DbSet<Household> Household { get; set; }
+    public DbSet<FoodItem> FoodItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<FoodItemCategory> FoodItemCategories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
---
+++ GreenMile/src/Web/Models/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class FoodItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public Household? Household { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ExpiryDate { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public string ImageFilePath { get; set; } = string.Empty;
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
