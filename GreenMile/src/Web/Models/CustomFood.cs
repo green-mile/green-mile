7cc11b6a0d35f3a5f2add6d85b1be2e13d4327c9
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2023-01-03 15:01:31 +0800

    Create custom food model and service

---
+++ GreenMile/src/Web/Models/CustomFood.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Web.Models;
+
+public class CustomFood
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public DateTime ExpiredDate { get; set; }
+    public Donation? Donation { get; set; }
+}
---
+++ GreenMile/src/Web/Services/CustomFoodService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MyCompany.Services
+{
+    public class CustomFoodService
+    {
+        private readonly DataContext _context;
+
+        public CustomFoodService(context)
+        {
+            _context = context;
+        }
+
+        public void AddCustomFood(CustomFood customfood)
+        {
+            _context.CustomFoods.add(customfood);
+            _context.savechanges();
+        }
+
+    }
+}
