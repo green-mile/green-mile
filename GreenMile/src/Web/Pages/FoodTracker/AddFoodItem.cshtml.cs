6a68c8b8a21ff61dee701a763c2661a3069993e5
Luth Andyka <luthandyka.business@gmail.com>
2023-01-03 22:04:07 +0800

    Create food item table and form, has bugs

---
+++ GreenMile/src/Web/Pages/FoodTracker/AddFoodItem.cshtml.cs
@@ -0,0 +1,58 @@
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Web.Data;
+using Web.Lib;
+using Web.Models;
+using Web.Services;
+
+namespace Web.Pages.FoodTracker
+{
+    public class AddFoodItemModel : PageModel
+    {
+        private readonly FoodItemService _fooditemService;
+        private readonly IHttpContextAccessor _http;
+        private readonly IHouseholdService _householdService;
+
+        public AddFoodItemModel(IHttpContextAccessor http, IHouseholdService householdService, FoodItemService fooditemService)
+        {
+            _http = http;
+            _householdService = householdService;
+            _fooditemService = fooditemService;
+        }
+
+        [BindProperty, Required, MinLength(1), MaxLength(20)]
+        public string Name { get; set; }
+        [BindProperty, Required, MinLength(0), MaxLength(100)]
+        public string Description { get; set; }
+        [BindProperty, Required, Range(1, 100, ErrorMessage = " Choose between 1 - 100")]
+        public int Quantity { get; set; }
+        [BindProperty, Required]
+        public string ExpiryDate { get; set; }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var householdName = _http.HttpContext?.Session.GetString(SessionVariable.HousholdName);
+                var household = (await _householdService.RetrieveHouseholdDetailsByName(householdName)).Value;
+                var newFood = new FoodItem()
+                {
+                    Household = household,
+                    Name = Name,
+                    Description = Description,
+                    Quantity = Quantity,
+                    ExpiryDate = ExpiryDate
+                };
+
+                _fooditemService.AddFoodItem(newFood);
+                return Redirect("/FoodTracker");
+            }
+            return Page();
+        }
+    }
+}
---
+++ GreenMile/src/Web/Pages/FoodTracker/Index.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Web.Data;
+using Web.Lib;
+using Web.Models;
+using Web.Services;
+
+namespace Web.Pages.FoodTracker
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly FoodItemService _fooditemService;
+        private readonly IHouseholdService _householdService;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        [BindProperty]
+        public string Id { get; set; }
+        public IEnumerable<FoodItem> FoodItemList { get; set; }
+
+
+        public IndexModel(FoodItemService fooditemService, IHouseholdService householdService, IHttpContextAccessor contextAccessor)
+        {
+            _fooditemService = fooditemService;
+            _householdService = householdService;
+            _contextAccessor = contextAccessor;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var householdName = _contextAccessor.HttpContext?.Session.GetString(SessionVariable.HousholdName);
+            var household = (await _householdService.RetrieveHouseholdDetailsByName(householdName)).Value;
+            FoodItemList = _fooditemService.GetAll(household);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            FoodItem? food = _fooditemService.GetFoodItemById(Id);
+            if (food != null)
+            {
+                _fooditemService.DeleteFoodItem(food);
+                return Redirect("/Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
